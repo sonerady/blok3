27397a5b6f95d24defa608fe9f49219151c01f95
Soner <80179760+sonerady@users.noreply.github.com>
2026-04-16 01:22:40 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://blok-3-server-production.up.railway.app',
         changeOrigin: true,
       },
     },
